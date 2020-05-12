4e9e757158a056f6f4e7b035dc685e8cbdadd24f
Glen Redden <glen.redden@studytafense.edu.au>
2020-05-13 08:53:21 +1000

    hey we commited!

--- Loops.js
+++ Loops.js
@@ -13,3 +13,19 @@ for (let i = 0; i < students.length; i++) {
 // console.log(students [i]);
 // }
 
+{  
+    public static void Main() 
+      {     
+          Console.WriteLine("Odd numbers from 1 to 99. Prints one number per line.");
+          for (int n = 1; n < (99 + 1); n++)
+            {
+                if (n % 2 != 0)
+                {
+                    Console.WriteLine(n.ToString());
+                }
+            }
+                    
+    }
+}
+
+
